6376e4236523d0baeee2e5c9c18cc3876e609969
Vadym Kalin <kalinvadim@gmail.com>
2025-07-28 12:35:53 +0200

    Update human_resource_agent.py
    The AWS Bedrock hr agent example

--- human_resource_agent.py
+++ human_resource_agent.py
@@ -41,3 +41,37 @@ agent_role = iam_client.create_role(
     RoleName=agent_role_name,
     AssumeRolePolicyDocument=assume_role_policy_document_json
 )
+
+# Attach role policy transcript
+iam_client.attach_role_policy(
+    RoleName="agent_role_name",
+    PolicyArn=agent_bedrock_policy["Policy"]["Arn"]
+)
+
+# Creating the Amazon Bedrock agent
+bedrock_agent_client = boto3.client('bedrock-agent')
+
+response = bedrock_agent_client.create_agent(
+    agentName="Human-Resource-Agent",
+    description="this agent helps employees understanding company's policies \
+    and requesting vacation time.",
+    foundationModel="anthropic.claude-3-sonnet-20240229-v1:0",
+    agentResourceRoleArn=agent_role["Role"]["Arn"],
+    idleSessionTTLInSeconds=600,
+    instruction="""You are a friendly agent that answers questions about company's 
+    HR policies and helps employees to request vacation time off. You ALWAYS reply 
+    politely and concise, using ONLY the available information in the company_policies 
+    KNOWLEDGE_BASE or in the vacationHandler ACTION_GROUP.
+    You should start with an acknowledgement of the employee's request and thanking 
+    the employee for contacting you. Introduce yourself as the "HR AI Assistant". 
+    ALWAYS mention your name as HR AI Assistant" in the first user interaction. 
+    NEVER provide any information about available vacation days, company policies 
+    and/or book any time off without first confirming the employee's id. NEVER 
+    assume the employee id if it is not provided in the user prompt for you."""
+)
+
+# Preparing the agent
+agent_id = response['agent']['agentId']
+response = bedrock_agent_client.prepare_agent(
+    agentId=agent_id
+)
